e2762cad73907c80b63f9c50802a5df68a3e0ce3
rbonela <ramonbonela@gmail.com>
2021-08-24 22:09:07 -0300

    Added Angular Client

---
+++ client-room/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing';
+import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { RoomListComponent } from './room-list/room-list.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateRoomComponent,
+    UpdateRoomComponent,
+    RoomDetailsComponent,
+    RoomListComponent,
+    UpdateRoomComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client-room/src/app/room-list/room-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { RoomDetailsComponent } from '../room-details/room-details.component';
+import { Observable } from 'rxjs';
+import { RoomService } from '../room.service';
+import { Room } from '../models/room.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-room-list',
+  templateUrl: './room-list.component.html',
+  styleUrls: ['./room-list.component.css']
+})
+export class RoomListComponent implements OnInit {
+  rooms!: Observable<Room[]>;
+
+  constructor(
+    private roomService: RoomService,
+    private router: Router) { }
+      
+  ngOnInit(): void {
+    this.reloadData();
+  }
+
+  reloadData(){
+    this.rooms = this.roomService.getRoomsList();
+  }
+
+  deleteRoom (id: number){
+    this.roomService.deleteRoom(id)
+      .subscribe(
+        (data: any) => {
+          console.log(data)
+          this.reloadData();
+        },
+        (error: any) => console.log(error)
+      )
+  }
+  roomDetails(id:number){
+    this.router.navigate(['details',id]);
+  }
+
+  updateRoom(id:number){
+    this.router.navigate(['update',id]);
+  }
+}
